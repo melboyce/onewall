433bc229d770e21136b369311b3c010738265c45
Mel Boyce <weird@syngin.net>
2017-10-01 17:52:43 +1100

    tidy up shitty old code

--- main.go
+++ main.go
@@ -1,80 +1,82 @@
-// onewall updates a single wallpaper entry in a `.fehbg` file.
-// a wallpaper is chosen randomly from a supplied directory and
-// is applied to the monitor referenced by the `pos` argument.
+// update a .fehbg file and refresh the xroot.
 package main
 
 import (
-    "flag"
-    "strings"
-    "time"
-    "os"
-
-    "io/ioutil"
-    "math/rand"
-    "os/exec"
-    "os/user"
-    "path/filepath"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-// init the random seed
 func init() {
-    rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+func bail(s string, e int) {
+	fmt.Fprintf(os.Stderr, s+"\n\n")
+	flag.Usage()
+	os.Exit(e)
+}
+
+func usage() {
+	fmt.Printf("usage: %s [-pos] <directory>\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+var pos = flag.Int("pos", 0, "position (xinerama screen index)")
+
 func main() {
-    pos := flag.Int("pos", 0, "position (xinerama index)")
-    dir := flag.String("dir", "", "directory to parse")
-    heads := flag.Int("heads", 1, "number of heads")
+	flag.Usage = usage
+	flag.Parse()
+	if flag.NArg() < 1 {
+		bail("no directory specified", 64)
+	}
 
-    flag.Parse()
+	dotfeh := fehBG()
+	if _, err := os.Stat(dotfeh); err != nil {
+		bail(err.Error(), 1)
+	}
+	b, err := ioutil.ReadFile(dotfeh)
+	if err != nil {
+		bail(err.Error(), 1)
+	}
+	curr := strings.Split(strings.TrimSpace(string(b)), " ")
+	curr = curr[3:] // TODO process each line to drop the shebang properly
+	if *pos > len(curr) {
+		bail("position greater than number of entries in .fehbg", 1)
+	}
+	for i, fp := range curr {
+		curr[i] = strings.Replace(fp, "'", "", -1)
+	}
 
-    if *dir == "" {
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	dir := flag.Arg(0)
+	walls, err := filepath.Glob(filepath.Join(dir, "*"))
+	if err != nil {
+		bail(err.Error(), 1)
+	}
+	curr[*pos] = walls[rand.Intn(len(walls))]
 
-    usr, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
+	args := []string{"--bg-fill"}
+	for _, fp := range curr {
+		args = append(args, fp)
+	}
+	cmd := exec.Command("feh", args...)
+	err = cmd.Run()
+	if err != nil {
+		bail(err.Error(), 2)
+	}
+}
 
-    // TODO handle non-existent .fehbg
-    fehpath := filepath.Join(usr.HomeDir, ".fehbg")
-    fehbg, err := ioutil.ReadFile(fehpath)
-    if err != nil {
-        panic(err)
-    }
-
-    bgs := strings.Split(string(fehbg), " ")
-    bgs = bgs[len(bgs) - *heads - 1:]
-
-    if *pos > len(bgs) - 1 {
-        panic("pos > heads")
-    }
-
-    patt := filepath.Join(*dir, "*")
-
-    walls, err := filepath.Glob(patt)
-    if err != nil {
-        panic(err)
-    }
-
-    fehArgs := []string{"--bg-fill"}
-    for i, w := range bgs {
-        w = strings.TrimSpace(w)
-        if len(w) > 0 {
-            if i == *pos {
-                fehArgs = append(fehArgs, walls[rand.Intn(len(walls))])
-            } else {
-                fehArgs = append(fehArgs, strings.Replace(w, "'", "", -1))
-            }
-        }
-    }
-
-    cmd := exec.Command("feh", fehArgs...)
-
-    err = cmd.Run()
-    if err != nil {
-        panic(err)
-    }
+func fehBG() string {
+	u, err := user.Current()
+	if err != nil {
+		bail(err.Error(), 1)
+	}
+	return filepath.Join(u.HomeDir, ".fehbg")
 }
