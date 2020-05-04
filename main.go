5abc6b98e6b5bedd433dac514981bb3d0653f34d
Mel Boyce <weird@syngin.net>
2020-05-04 20:19:09 +1000

    reimplement

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
-// update a .fehbg file and refresh the xroot.
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -18,65 +18,94 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
-func bail(s string, e int) {
-	fmt.Fprintf(os.Stderr, s+"\n\n")
-	flag.Usage()
-	os.Exit(e)
-}
-
-func usage() {
-	fmt.Printf("usage: %s [-pos] <directory>\n", os.Args[0])
-	flag.PrintDefaults()
-}
-
-var pos = flag.Int("pos", 0, "position (xinerama screen index)")
-
 func main() {
+	pos := flag.Int("pos", 0, "position (xinerama index)")
 	flag.Usage = usage
 	flag.Parse()
 	if flag.NArg() < 1 {
-		bail("no directory specified", 64)
+		usage()
+		os.Exit(64)
 	}
-
-	dotfeh := fehBG()
-	if _, err := os.Stat(dotfeh); err != nil {
-		bail(err.Error(), 1)
-	}
-	b, err := ioutil.ReadFile(dotfeh)
-	if err != nil {
-		bail(err.Error(), 1)
-	}
-	curr := strings.Split(strings.TrimSpace(string(b)), " ")
-	curr = curr[3:] // TODO process each line to drop the shebang properly
-	if *pos > len(curr) {
-		bail("position greater than number of entries in .fehbg", 1)
-	}
-	for i, fp := range curr {
-		curr[i] = strings.Replace(fp, "'", "", -1)
-	}
-
 	dir := flag.Arg(0)
-	walls, err := filepath.Glob(filepath.Join(dir, "*"))
+
+	user, err := user.Current()
 	if err != nil {
-		bail(err.Error(), 1)
+		fmt.Fprintf(os.Stderr, "cant get user: %v\n", err)
+		os.Exit(1)
 	}
-	curr[*pos] = walls[rand.Intn(len(walls))]
+
+	fehbg := filepath.Join(user.HomeDir, ".fehbg")
+	f, err := os.Open(fehbg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cant open %s: %v\n", fehbg, err)
+		os.Exit(2)
+	}
+	defer f.Close()
+
+	feh, err := getCmd(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing %s: %v\n", fehbg, err)
+		os.Exit(3)
+	}
+
+	walls := []string{}
+	els := strings.Fields(feh)
+	for _, el := range els {
+		if el == "feh" || strings.HasPrefix(el, "-") {
+			continue
+		}
+		el = strings.Trim(el, "'")
+		walls = append(walls, el)
+	}
+
+	nwall, err := getWall(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cant get wall from %s: %v\n", dir, err)
+		os.Exit(4)
+	}
+
+	if *pos > len(walls)-1 {
+		walls = append(walls, nwall)
+	} else {
+		walls[*pos] = nwall
+	}
 
 	args := []string{"--bg-fill"}
-	for _, fp := range curr {
-		args = append(args, fp)
+	for _, wall := range walls {
+		args = append(args, wall)
 	}
 	cmd := exec.Command("feh", args...)
 	err = cmd.Run()
 	if err != nil {
-		bail(err.Error(), 2)
+		fmt.Fprintf(os.Stderr, "cant run feh: %v\n", err)
+		os.Exit(5)
 	}
+	os.Exit(0)
 }
 
-func fehBG() string {
-	u, err := user.Current()
+func getWall(d string) (string, error) {
+	walls, err := filepath.Glob(filepath.Join(d, "*"))
 	if err != nil {
-		bail(err.Error(), 1)
+		return "", fmt.Errorf("cant get walls: %w", err)
 	}
-	return filepath.Join(u.HomeDir, ".fehbg")
+	return walls[rand.Intn(len(walls))], nil
+}
+
+func getCmd(f *os.File) (string, error) {
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		line := sc.Text()
+		if strings.HasPrefix(line, "feh ") {
+			return line, nil
+		}
+	}
+	if err := sc.Err(); err != nil {
+		return "", fmt.Errorf("error scanning file: %w", err)
+	}
+	return "", errors.New("cant find feh command in file")
+}
+
+func usage() {
+	fmt.Printf("usage: %s [-pos N] <directory>\n", os.Args[0])
+	flag.PrintDefaults()
 }
